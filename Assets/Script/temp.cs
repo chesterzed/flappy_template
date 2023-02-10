a3e6294420d0445ce12b37cc9b038d0530783778
Ratibor Vanin <chester.1.80@mail.ru>
2023-02-10 18:16:02 +0300

    temp

---
+++ Assets/Script/temp.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class temp : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnMove(Vector2 direction)
+    {
+        rb.velocity = new Vector3(
+            direction.x, 
+            rb.velocity.y, 
+            direction.y
+            );
+        
+    }
+}
